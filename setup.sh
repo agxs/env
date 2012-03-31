daf0afa60f6db0f7e3e409fe4a32cdf2c8377a29
Andrew Seales <agxs@frozenmercury.co.uk>
2012-03-31 11:37:43 +0100

    Added gnome-terminal settings and setup.sh removes old links.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+rm $HOME/.bashrc_as \
+   $HOME/.vimrc \
+   $HOME/.inputrc \
+   $HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml
+
 ln -s $HOME/src/env/bashrc $HOME/.bashrc_as
 ln -s $HOME/src/env/vimrc $HOME/.vimrc
 ln -s $HOME/src/env/inputrc $HOME/.inputrc
+ln -s $HOME/src/env/%gconf.xml \
+      $HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml
 
