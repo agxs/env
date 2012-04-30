59fe60e3dc8179170be419461db0c18cc7947bf7
Andrew Seales <andrew.seales@ed.ac.uk>
2012-04-30 11:57:03 +0100

    Added irssi theme/config that works with Solarized colours.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ rm $HOME/.bashrc_as \
 ln -s $HOME/src/env/bashrc $HOME/.bashrc_as
 ln -s $HOME/src/env/vimrc $HOME/.vimrc
 ln -s $HOME/src/env/inputrc $HOME/.inputrc
+ln -s $HOME/src/env/solarized-universal.theme $HOME/.irssi/solarized-universal.theme
 
 # Sets up gnome-terminal
 if [ -n "`which gconftool-2`" ]; then
