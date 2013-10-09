8255abffd9a97688fbdb055677f5dddac8360d65
Andrew Seales <andrew.seales@ed.ac.uk>
2013-10-09 08:29:57 +0100

    Added more tests to setup.sh to check for folder existence.

--- setup.sh
+++ setup.sh
@@ -3,14 +3,16 @@
 ln -sf $HOME/src/env/bashrc $HOME/.bashrc_as
 ln -sf $HOME/src/env/vimrc.local $HOME/.vimrc.local
 ln -sf $HOME/src/env/inputrc $HOME/.inputrc
-ln -sf $HOME/src/env/solarized-universal.theme $HOME/.irssi/solarized-universal.theme
-ln -sf $HOME/src/env/irssi_config $HOME/.irssi/config
 ln -sf $HOME/src/env/zshrc $HOME/.zshrc
 ln -sf $HOME/src/env/agxs.zsh-theme $HOME/.oh-my-zsh/themes/agxs.zsh-theme
 ln -sf $HOME/src/env/tmux.conf $HOME/.tmux.conf
+if [[ -d "$HOME/.irssi" ]]; then
+  ln -sf $HOME/src/env/solarized-universal.theme $HOME/.irssi/solarized-universal.theme
+  ln -sf $HOME/src/env/irssi_config $HOME/.irssi/config
+fi
 
 # Sets up gnome-terminal
-if [ -n "`which gconftool-2`" ]; then
+if [ -e "`which gconftool-2`" ]; then
   gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_background" --type bool false
   gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --type bool false
   gconftool-2 --set "/apps/gnome-terminal/profiles/Default/palette" --type string "#070736364242:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
