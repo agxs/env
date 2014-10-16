b8c012c7e7791f860ac45676307f7d20fbc32a2e
Andrew Seales <andrew.seales@ed.ac.uk>
2014-10-16 15:13:57 +0100

    Fix vagrant plugin.

--- vagrant.plugin.zsh
+++ vagrant.plugin.zsh
@@ -1,6 +1,6 @@
 function vagrant_state() {
   if [[ -e Vagrantfile ]]; then
-    v_stat=`vagrant status 2>/dev/null | grep default | awk '{print $2}'`
+    v_stat=`vagrant status 2>/dev/null | sed -n 3p | awk '{print $2}'`
   fi
   echo $v_stat
 }
