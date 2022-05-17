7e2628e9230111cbb28ecda0882828c56104d7fb
Andrew Seales <andrew.seales@ed.ac.uk>
2022-05-17 08:45:22 +0100

    Add multi-ssh script using tmux

---
+++ bin/multi-ssh.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ssh-multi
+# D.Kovalov
+# Based on http://linuxpixies.blogspot.jp/2011/06/tmux-copy-mode-and-how-to-control.html
+
+# a script to ssh multiple servers over multiple tmux panes
+
+
+starttmux() {
+    if [ -z "$HOSTS" ]; then
+       echo -n "Please provide of list of hosts separated by spaces [ENTER]: "
+       read HOSTS
+    fi
+
+    local hosts=( $HOSTS )
+
+
+    tmux new-window "ssh ${USER}@${hosts[0]}"
+    unset hosts[0];
+    for i in "${hosts[@]}"; do
+        tmux split-window -h  "ssh ${USER}@$i"
+        tmux select-layout tiled > /dev/null
+    done
+    tmux select-pane -t 0
+    tmux set-window-option synchronize-panes on > /dev/null
+
+}
+
+HOSTS=${HOSTS:=$*}
+USER=${USER:=aseales}
+
+starttmux
+
