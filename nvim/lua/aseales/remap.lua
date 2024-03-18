dd9547f4985a29597ae58e97d7883d96becbb03d
Andrew Seales <andrew.seales@ed.ac.uk>
2024-03-18 16:22:56 +0000

    Add comments to nvim & disable cursors

--- nvim/lua/aseales/remap.lua
+++ nvim/lua/aseales/remap.lua
@@ -1,33 +1,49 @@
 vim.g.mapleader = " "
 
+vim.keymap.set({ "n", "v" }, "<Up>", "<Nop>", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Down>", "<Nop>", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Left>", "<Nop>", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Right>", "<Nop>", { noremap = true })
+
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 
+-- opens file browser
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- opens undo tree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
+-- comment toggle
 vim.keymap.set("n", "<leader>/", vim.cmd.CommentToggle)
 vim.keymap.set("v", "<leader>/", ":'<,'>CommentToggle<CR>gv")
 
+-- moves highlighted code up and down and reformats
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- append line below but with a space and doesn't move cursor
 vim.keymap.set("n", "J", "mzJ`z")
+-- page up/down but keeps cursor in one place
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- when search skipping keeps the cursor in the middle
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- don't overwrite paste buffer when pasting over highlight
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- yank to system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+-- deletes to void instead of default register
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- format code using lsp
 vim.keymap.set("n", "<leader>f", function()
   vim.lsp.buf.format()
 end)
@@ -36,5 +52,5 @@ end)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- expands the current file dir
-vim.keymap.set('c', '%%', [[<C-R>=fnameescape(expand('%:h')).'/'<cr>]])
+vim.keymap.set("c", '%%', [[<C-R>=fnameescape(expand('%:h')).'/'<cr>]])
 
