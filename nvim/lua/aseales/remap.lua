dac202fbb867fdde059db83fd2f339f8435ed30c
Andrew Seales <agxs@frozenmercury.co.uk>
2024-04-29 15:27:07 +0100

    Add paste from system clipboard

--- nvim/lua/aseales/remap.lua
+++ nvim/lua/aseales/remap.lua
@@ -39,6 +39,8 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>P", "\"+P")
 
 -- deletes to void instead of default register
 vim.keymap.set("n", "<leader>d", "\"_d")
