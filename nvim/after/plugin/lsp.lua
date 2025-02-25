4db324024897d7dc8cc15c11c7209248956b639d
Andrew Seales <agxs@frozenmercury.co.uk>
2025-02-25 08:13:10 +0000

    Only install lua lsp by default in nvim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -18,7 +18,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {'ts_ls', 'rust_analyzer', 'lua_ls'},
+	ensure_installed = {'lua_ls'},
 	handlers = {
 		lsp_zero.default_setup,
 		lua_ls = function()
