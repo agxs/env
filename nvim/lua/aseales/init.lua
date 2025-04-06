b9c122fa6f9a8f5a048e6d296a412be63f08fe7d
Andrew Seales <agxs@frozenmercury.co.uk>
2025-04-06 10:44:40 +0100

    Fix helm template highlighting in neovim

--- nvim/lua/aseales/init.lua
+++ nvim/lua/aseales/init.lua
@@ -1,3 +1,14 @@
 require("aseales.lazy")
 require("aseales.remap")
 require("aseales.set")
+
+vim.filetype.add({
+  extension = {
+    gotmpl = 'gotmpl',
+  },
+  pattern = {
+    [".*/templates/.*%.tpl"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+    ["helmfile.*%.ya?ml"] = "helm",
+  },
+})
