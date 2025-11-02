2d75809bffe9dfdfe5dc0447eacb44cfb1a03a22
Andrew Seales <agxs@frozenmercury.co.uk>
2025-11-02 13:11:21 +0000

    Add arch config to arch setup files

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,7 +1,14 @@
 sudo pacman -S \
+  bind \
   firefox \
   git-delta \
   starship \
   tmux \
   vim \
   zsh
+
+ln -sf ~/src/env/arch/alacritty $HOME/.config/alacritty
+ln -sf ~/src/env/arch/waybar $HOME/.config/waybar
+ln -sf ~/src/env/arch/hypr $HOME/.config/hypr
+
+
