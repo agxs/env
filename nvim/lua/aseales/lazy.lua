729af1110963a82b19680295293330a3de5bf88f
Andrew Seales <agxs@frozenmercury.co.uk>
2026-01-25 14:50:19 +0000

    Fix treesitter upgrade

--- nvim/lua/aseales/lazy.lua
+++ nvim/lua/aseales/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -24,17 +24,18 @@ vim.g.maplocalleader = " "
 return require('lazy').setup({
 
   {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  dependencies = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    -- or                            , branch = '0.1.x',
+    dependencies = { { 'nvim-lua/plenary.nvim' } }
   },
 
   {
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
   },
 
   {
@@ -44,8 +45,7 @@ return require('lazy').setup({
     opts = {},
   },
 
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  'nvim-treesitter/playground',
+  { 'nvim-treesitter/nvim-treesitter', lazy = false, build = ':TSUpdate' },
   'theprimeagen/harpoon',
   'mbbill/undotree',
   'tpope/vim-rhubarb',
@@ -53,25 +53,25 @@ return require('lazy').setup({
   'shumphrey/fugitive-gitlab.vim',
 
   {
-	  'VonHeikemen/lsp-zero.nvim',
-	  dependencies = {
-		  -- LSP
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    dependencies = {
+      -- LSP
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
   },
 
   "terrortylor/nvim-comment",
@@ -81,7 +81,7 @@ return require('lazy').setup({
   {
     "folke/trouble.nvim",
     dependencies = {
-      {'nvim-tree/nvim-web-devicons'}
+      { 'nvim-tree/nvim-web-devicons' }
     },
   },
 })
