8f83d9409eb90553d700958aa5b27c0407279848
Andrew Seales <agxs@frozenmercury.co.uk>
2026-04-03 09:50:22 +0100

    Fix deprecated api in nvim config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -29,7 +29,7 @@ local on_attach = lsp_zero.on_attach(function(client, bufnr)
     vim.lsp.buf.format()
   end)
 
-  if client.supports_method("textDocument/formatting") then
+  if client:supports_method("textDocument/formatting") then
     vim.api.nvim_clear_autocmds({
       group = augroup,
       buffer = bufnr,
